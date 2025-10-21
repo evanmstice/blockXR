3e637e7233b2148258d3f2ca8c8f1e6c20a2a290
Evan <estice@ufl.edu>
2025-10-21 11:36:30 -0400

    [FIX]: made predict.py a callable function

--- predict.py
+++ predict.py
@@ -12,36 +12,38 @@ model = YOLO("block_weights.pt")
 # Testing with a single photo
 results = model.predict(source="test.png", save=True, conf=0.5)
 
-blocks = []
+def getBlocks(data) -> list[Block]:
 
-# Extracting bounding box data and loading into Block class objects
-detected = results[0].boxes
-for block in detected:
-    coordinates = block.xyxy.tolist()[0]
-    xPos = coordinates[0] + (coordinates[2] - coordinates[0]) / 2  # centered x position of block
-    blocks.append(Block(model.names[int(block.cls[0].item())], xPos, coordinates[1], coordinates[3]))
+    blocks = []
 
-blocks.sort(key=lambda b: b.yPosTop)
+    # Extracting bounding box data and loading into Block class objects
+    # data: detected = results[0].boxes
+    for block in data:
+        coordinates = block.xyxy.tolist()[0]
+        xPos = coordinates[0] + (coordinates[2] - coordinates[0]) / 2  # centered x position of block
+        blocks.append(Block(model.names[int(block.cls[0].item())], xPos, coordinates[1], coordinates[3]))
 
-toleranceX = 75
-toleranceY = 10  # Add later: auto tolerance, could start program by checking width of blocks in relation to camera view and then
-                # have tolerance be a percentage of that
+    blocks.sort(key=lambda b: b.yPosTop)
 
-codeBlocksFinal = []
-whenClickedIndice = next((i for i, b in enumerate(blocks) if b.name == "When clicked"), None)
-if whenClickedIndice is None:
-    raise KeyError("Error: When clicked block not found.")
+    toleranceX = 75
+    toleranceY = 10  # Add later: auto tolerance, could start program by checking width of blocks in relation to camera view and then
+                    # have tolerance be a percentage of that
 
-codeBlocksFinal.append("When clicked")
-currXPos = blocks[whenClickedIndice].xPos
-currYPos = blocks[whenClickedIndice].yPosBottom
-for block in blocks[whenClickedIndice + 1:]:
-    # check if block below is snapped together
-    if (currYPos - toleranceY <= block.yPosTop <= currYPos + toleranceY) and (currXPos - toleranceX <= block.xPos <= currXPos + toleranceX):
-        codeBlocksFinal.append(block.name)
-        currYPos = block.yPosBottom
-        currXPos = block.xPos
-    else:
-        break
+    codeBlocksFinal = []
+    whenClickedIndice = next((i for i, b in enumerate(blocks) if b.name == "When clicked"), None)
+    if whenClickedIndice is None:
+        raise KeyError("Error: When clicked block not found.")
 
-print(codeBlocksFinal)
+    codeBlocksFinal.append("When clicked")
+    currXPos = blocks[whenClickedIndice].xPos
+    currYPos = blocks[whenClickedIndice].yPosBottom
+    for block in blocks[whenClickedIndice + 1:]:
+        # check if block below is snapped together
+        if (currYPos - toleranceY <= block.yPosTop <= currYPos + toleranceY) and (currXPos - toleranceX <= block.xPos <= currXPos + toleranceX):
+            codeBlocksFinal.append(block.name)
+            currYPos = block.yPosBottom
+            currXPos = block.xPos
+        else:
+            break
+
+    return codeBlocksFinal
