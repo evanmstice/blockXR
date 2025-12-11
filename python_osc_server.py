672a285d50d892e7406f1e7f6454403890f97007
rehamansoor <reha.mansoor@ufl.edu>
2025-12-11 00:18:34 -0500

    Implementation for communication between unity and python model

---
+++ python_osc_server.py
@@ -0,0 +1,24 @@
+from pythonosc.dispatcher import Dispatcher
+from pythonosc import osc_server
+from pythonosc.udp_client import SimpleUDPClient
+
+import cv2
+from detect import detect_blocks
+
+def req_handler(addr, *args):
+    
+    #blocks = detect_blocks()
+    blocks = ["move_forward", "turn_left", "turn_right"]
+
+    client = SimpleUDPClient('127.0.0.1', 7001)
+    client.send_message("/program", blocks)
+
+
+dispatcher = Dispatcher()
+# if an osc message is received on address /req, call req_handler
+dispatcher.map("/req", req_handler)
+
+# creates a server that listens for message on the given address on the given port
+server = osc_server.ThreadingOSCUDPServer(("127.0.0.1", 31415), dispatcher)
+print("Serving on {}".format(server.server_address))
+server.serve_forever()
